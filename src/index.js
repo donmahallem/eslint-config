bcd2695925960790388ee62269c9000c6c09c088
donmahallem <donmahallem@users.noreply.github.com>
2025-02-09 11:18:22 +0100

    feat(deps): add eslint-plugin-chai-friendly

--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
 import mochaPlugin from 'eslint-plugin-mocha';
+import pluginChaiFriendly from 'eslint-plugin-chai-friendly';
 
-export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended, mochaPlugin.configs.flat.recommended);
+export default tseslint.config(
+    eslint.configs.recommended,
+    tseslint.configs.recommended,
+    mochaPlugin.configs.flat.recommended,
+    pluginChaiFriendly.configs.recommendedFlat
+);
